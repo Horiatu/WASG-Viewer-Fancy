9833dfb011965eaa7dbdcf2ad1c624f4a1259d93
Horia Tudosie <htudosie@esri.ca>
2018-01-12 13:09:56 -0500

    fetch

--- sw.js
+++ sw.js
@@ -1,12 +1,14 @@
 var CACHE_NAME = 'WCAG-Fancy-cache-v1';
 var urlsToCache = [
   './',
-  './main.css',
   './main.js',
-  './icomoon/fonts/icomoon.eot',
-  './icomoon/fonts/icomoon.svg', 
-  './icomoon/fonts/icomoon.ttf',
-  './icomoon/fonts/icomoon.woff'
+  './main.css'
+  // './images/logo.png',
+  // './images/flag.CA.22.png',
+  // './icomoon/fonts/icomoon.eot',
+  // './icomoon/fonts/icomoon.svg', 
+  // './icomoon/fonts/icomoon.ttf',
+  // './icomoon/fonts/icomoon.woff'
 ];
 
 self.addEventListener('install', function(event) {
@@ -17,5 +19,45 @@ self.addEventListener('install', function(event) {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  // console.log('request: ', event.request.url);
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // console.log('match: ', event.request.url);
+        // Cache hit - return response
+        if (response) {
+          // console.log('response');
+          return response;
+        }
+        // return fetch(event.request);
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      }
+    )
   );
 });
