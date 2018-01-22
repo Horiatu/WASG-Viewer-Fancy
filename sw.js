0d86767793359690ccdc240e8ff8e6eec24151f7
Horia Tudosie <htudosie@esri.ca>
2018-01-22 14:46:10 -0500

    clear cache

--- sw.js
+++ sw.js
@@ -8,52 +8,52 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
-          // caches.delete(CACHE_NAME).then(function() {
+        caches.delete(CACHE_NAME).then(function() {
           console.log('Opened cache: ', CACHE_NAME, cache.keys());
-        // });
+        });
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-self.addEventListener('fetch', function(event) {
-  // console.log('fetch ',event);
-  event.respondWith(
-    fetch(event.request).then(function(response) {
-      console.log('response status ', response.status);
-      if(response && response.status <= 400) {
-        // console.log('cache');
-        var responseToCache = response.clone();
+// self.addEventListener('fetch', function(event) {
+//   // console.log('fetch ',event);
+//   event.respondWith(
+//     fetch(event.request).then(function(response) {
+//       console.log('response status ', response.status);
+//       if(response && response.status <= 400) {
+//         // console.log('cache');
+//         var responseToCache = response.clone();
 
-        caches.open(CACHE_NAME)
-          .then(function(cache) {
-            console.log("Add to Cache: ", response.status, responseToCache);
+//         caches.open(CACHE_NAME)
+//           .then(function(cache) {
+//             console.log("Add to Cache: ", response.status, responseToCache);
 
-            cache.put(event.request, responseToCache);
-          });
-        return response;
-        }
-      else 
-        { 
-          // error: get from cache
-          caches.open(CACHE_NAME).then(function(cache) {
+//             cache.put(event.request, responseToCache);
+//           });
+//         return response;
+//         }
+//       else 
+//         { 
+//           // error: get from cache
+//           caches.open(CACHE_NAME).then(function(cache) {
 
-            caches.match(event.request).then(function(response) {
-              // Cache hit - return response
-              if (response) {
-                console.log("From Cache: ", response);
-                return response;
-              }
-            }, function(reason) {
-              return null;
-            });
-          });
-        }
-      }, function(reason) {
-        console.log('Featch error with reason ', reason);
-      })
-    );
-});
+//             caches.match(event.request).then(function(response) {
+//               // Cache hit - return response
+//               if (response) {
+//                 console.log("From Cache: ", response);
+//                 return response;
+//               }
+//             }, function(reason) {
+//               return null;
+//             });
+//           });
+//         }
+//       }, function(reason) {
+//         console.log('Featch error with reason ', reason);
+//       })
+//     );
+// });
 
 // self.addEventListener('fetch', function(event) {
 //   event.respondWith(
